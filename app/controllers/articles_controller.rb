2122f7bc149b2130d77f4ca56fc8a24f6965c6e1
Tania Villanueva <trodvil112@gmail.com>
2023-11-07 10:11:30 -0600

    error form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,13 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        
     end
   
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         @article.save
-        redirect_to @article
+        render @article
     end
 end
