59baf177cc4fcdbdbfbbf7dfa4889f7155933dba
Tania Villanueva <trodvil112@gmail.com>
2024-02-14 12:19:07 -0600

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,43 @@
 class UsersController < ApplicationController
-    def show
-      @user = User.find(params[:id])
-      @articles = @user.articles
-    end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
 
-    def index
-      @users = User.all
-    end
-    def new
-        @user = User.new
-    end
+  def index
+    @users = User.all
+  end
+  def new
+      @user = User.new
+  end
 
-    def edit
-      @user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
-  
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your account information was successfully updated"
-        redirect_to @user
+  end
+
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+        redirect_to articles_path
       else
-        render 'edit'
+        render 'new'
       end
     end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-          redirect_to articles_path
-        else
-          render 'new'
-        end
-      end
 
-
-      private
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+    private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
