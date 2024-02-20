ae483966a010e96e7c53da114dd229a47846bbf5
Tania Villanueva <trodvil112@gmail.com>
2024-02-20 16:51:43 -0600

    Controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
     include Pagy::Frontend
+
+    
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    
+    def logged_in?
+        !!current_user
+    end
+ 
 end
